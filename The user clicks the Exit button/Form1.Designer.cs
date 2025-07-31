254db6f3aa714bac7da3b29a9ad978020e8999bc
Yang Naden <168646003+Nadencoder@users.noreply.github.com>
2025-08-01 04:03:35 +0700

    Add files via upload

---
+++ The user clicks the Exit button/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace The_user_clicks_the_Exit_button
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.discountPct = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSubtotal = new System.Windows.Forms.TextBox();
+            this.txtDiscontPct = new System.Windows.Forms.TextBox();
+            this.txtDiscontAmt = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Subtotal";
+            // 
+            // discountPct
+            // 
+            this.discountPct.AutoSize = true;
+            this.discountPct.Location = new System.Drawing.Point(224, 139);
+            this.discountPct.Name = "discountPct";
+            this.discountPct.Size = new System.Drawing.Size(101, 16);
+            this.discountPct.TabIndex = 1;
+            this.discountPct.Text = "Discont Percent";
+            this.discountPct.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Discont Amount";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(224, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Total";
+            // 
+            // txtSubtotal
+            // 
+            this.txtSubtotal.Location = new System.Drawing.Point(345, 87);
+            this.txtSubtotal.Name = "txtSubtotal";
+            this.txtSubtotal.Size = new System.Drawing.Size(100, 22);
+            this.txtSubtotal.TabIndex = 4;
+            // 
+            // txtDiscontPct
+            // 
+            this.txtDiscontPct.Location = new System.Drawing.Point(345, 139);
+            this.txtDiscontPct.Name = "txtDiscontPct";
+            this.txtDiscontPct.Size = new System.Drawing.Size(100, 22);
+            this.txtDiscontPct.TabIndex = 5;
+            // 
+            // txtDiscontAmt
+            // 
+            this.txtDiscontAmt.Location = new System.Drawing.Point(345, 192);
+            this.txtDiscontAmt.Name = "txtDiscontAmt";
+            this.txtDiscontAmt.Size = new System.Drawing.Size(100, 22);
+            this.txtDiscontAmt.TabIndex = 6;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(345, 237);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 22);
+            this.txtTotal.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(224, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calulator";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(370, 321);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtDiscontAmt);
+            this.Controls.Add(this.txtDiscontPct);
+            this.Controls.Add(this.txtSubtotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.discountPct);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label discountPct;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSubtotal;
+        private System.Windows.Forms.TextBox txtDiscontPct;
+        private System.Windows.Forms.TextBox txtDiscontAmt;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ The user clicks the Exit button/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_user_clicks_the_Exit_button
+{
+    public partial class Form1 : Form
+    {
+        private EventHandler btnExit_click;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnExit.Click += btnExit_click;
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+           
+            this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+             * This is method calculate the total 
+             * For an invoice depending on a discont that's based on the subtotal
+            */
+
+            //get the subtotal amount from the Subtotal txt box
+            decimal subtotal = Convert.ToDecimal(txtSubtotal.Text);
+
+            //Set the discont varirable based  on the value of the subtotal variable
+            decimal discontPct = 0m;
+            if (subtotal >= 500)
+            {
+                discontPct = .2m;
+            }
+            else if (subtotal >= 250 && subtotal < 500)
+            {
+                discontPct = .15m;
+            }
+            else if (subtotal >= 100 && subtotal < 250)
+            {
+                discontPct = .1m;
+            }
+
+            //Calculate and assign the value for the discont Amt and invoiceTotal variables
+            decimal discountAmt = subtotal * discontPct;
+            decimal invoiceTotal = subtotal - discountAmt;
+
+            //Fotmat the values and display them in thier text boxes
+            txtDiscontPct.Text = discontPct.ToString("p1");//percent format with 1 decimal place
+            txtDiscontAmt.Text = invoiceTotal.ToString("c");//currency format
+            txtTotal.Text = invoiceTotal.ToString("c"); 
+
+            //move the focus to the subtotal text box
+
+            txtSubtotal.Focus();
+            
+        }
+    }
+}
